afdb96bc97c2d59e1e1f735633ca93f20dd1dc5b
devashishkd <devashishkumar693@example.com>
2026-02-19 22:55:11 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
+import roomRouter from "./routes/roomRoutes.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/api/auth", authRouter);
+app.use("/api/rooms", roomRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
